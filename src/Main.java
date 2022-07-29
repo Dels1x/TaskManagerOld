e620be6a0bab377fc139f86a10b09a25d620f7c2
Danislav <delsixreborn@gmail.com>
2022-07-29 12:44:36 +0300

    Start of the 2nd day. Switch panels functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        new TaskManagerApp();
+        JFrame frame = new JFrame("TaskManagerApp");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new TaskManagerApp().mainPanel);
+        frame.pack();
+        frame.setSize(500, 500);
     }
 }
--- src/TaskManagerApp.java
+++ src/TaskManagerApp.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TaskManagerApp {
     private JButton completedButton;
     private JButton uncompletedButton;
     private JPanel switchPanelsPanel;
+    JPanel mainPanel;
+    private JPanel switchPanel;
+    private JPanel uncompletedPanel;
+    private JPanel completedPanel;
+    private JLabel completedLabel;
+    private JLabel uncompletedLabel;
+
+    public TaskManagerApp() {
+        completedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                uncompletedPanel.setVisible(false);
+                completedPanel.setVisible(true);
+            }
+        });
+        uncompletedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                uncompletedPanel.setVisible(true);
+                completedPanel.setVisible(false);
+            }
+        });
+    }
 }
